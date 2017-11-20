c432f00812163c79707576e80043101cfeda31ec
Abdelrahman1996 <akmaned@gmail.com>
2017-11-19 20:35:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
-from flask_restful import Api, Resource
+import os
+
+from flask import Flask
+from flask_restful import Api
 from flask_jwt import JWT
 
 from security import authenticate, identity
@@ -7,30 +9,31 @@ from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
-app = Flask(__name__, template_folder='template')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-app.config['SQLALCHEMY_TRACKS_MODIFICATIONS'] = False
+app = Flask(__name__)
+
+app.config['DEBUG'] = True
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
 
-jwt = JWT(app, authenticate, identity)
+jwt = JWT(app, authenticate, identity)  # /auth
 
-class Web_Page(Resource):
-    def get(self):
-        return render_template('index.html')
-
-@app.before_first_request
-def create_tables():
-   db.create_all()
-         
-api.add_resource(Web_Page, '/')
-api.add_resource(UserRegister, '/register')
+api.add_resource(Store, '/store/<string:name>')
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
-api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
+
+api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
     from db import db
     db.init_app(app)
-    app.run(port=8080,debug=True)  # important to mention debug=True
+
+    if app.config['DEBUG']:
+        @app.before_first_request
+        def create_tables():
+            db.create_all()
+
+    app.run(port=5000)
