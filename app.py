bc047d6bb0f65ee5e5ea40b90bec3c85b695f8e6
Abdelrahman1996 <akmaned@gmail.com>
2017-11-19 20:29:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from flask_restful import Api
+from flask_restful import Api, Resource
 from flask_jwt import JWT
 
 from security import authenticate, identity
@@ -7,7 +7,7 @@ from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='template')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACKS_MODIFICATIONS'] = False
 app.secret_key = 'jose'
@@ -15,10 +15,15 @@ api = Api(app)
 
 jwt = JWT(app, authenticate, identity)
 
+class Web_Page(Resource):
+    def get(self):
+        return render_template('index.html')
+
 @app.before_first_request
 def create_tables():
    db.create_all()
          
+api.add_resource(Web_Page, '/')
 api.add_resource(UserRegister, '/register')
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
