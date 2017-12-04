d3ca3f3495da3cb19405ea06118c4bb1ea9456d0
Abdelrahman1996 <akmaned@gmail.com>
2017-12-04 14:08:54 -0500

    Update security.py

--- security.py
+++ security.py
@@ -1,9 +1,13 @@
 from werkzeug.security import safe_str_cmp
 from models.user import UserModel
+from passlib.apps import custom_app_context as pwd_context
+
+def hash_password(password):
+    self.password_hash = pwd_context.encrypt(password)
 
 def authenticate(username, password):
     user = UserModel.find_by_username(username)
-    if user and safe_str_cmp(user.password, password):
+    if user and pwd_context.verify(user.password, hash_password(password)):
         return user
 
 def identity(payload):
