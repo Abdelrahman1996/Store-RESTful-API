e4348f975c349dba6e17286e8c7422500a984f40
Abdelrahman1996 <akmaned@gmail.com>
2017-12-05 14:17:35 -0500

    Update security.py

--- security.py
+++ security.py
@@ -7,8 +7,8 @@ def hash_password(password):
 
 def authenticate(username, password):
     user = UserModel.find_by_username(username)
-    print user.password
-    print hash_password(password)
+    print (user.password)
+    print (hash_password(password))
     if user and safe_str_cmp(user.password.encode('utf-8'), hash_password(password).encode('utf-8')):
         return user
 
